da40d02bd45b4ad879d516dc971120e1367fe1eb
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-04-11 03:02:00 +0900

    [Feat]: getOption Promise 적용

--- public/contents/content.js
+++ public/contents/content.js
@@ -1,15 +1,27 @@
+/*eslint-disable*/
+
 window.addEventListener('load', () => {
-  const handleOpenPage = (url) => {
-    window.open(url, '_blank');
+  const handleOpenPage = (url, option) => {
+    window.open(url, option);
   };
 
-  const handleClick = (event) => {
+  const getOption = async () => {
+    return new Promise((resolve) => {
+      chrome.storage.local.get(['targetOption'], (result) => {
+        resolve(result.targetOption);
+      });
+    });
+  };
+
+  const handleClick = async (event) => {
     const anchorEl = event.target.closest('a');
 
-    if (anchorEl && anchorEl.href) {
+    if (anchorEl && anchorEl.href && !anchorEl.href.endsWith('#')) {
       event.preventDefault();
       event.stopImmediatePropagation();
-      handleOpenPage(anchorEl.href);
+
+      const option = await getOption();
+      handleOpenPage(anchorEl.href, option);
     }
   };
 
