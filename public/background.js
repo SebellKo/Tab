6165399d6b600f694ca15feea71d035580a2133c
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-04-10 21:12:43 +0900

    [Feat]: 초기 타겟 옵션 수정

--- public/background.js
+++ public/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
-    chrome.storage.local.set({ targetOption: '_self' });
+    chrome.storage.local.set({ targetOption: '_parent' });
   }
 });
