9326371ba2ea35fe46c24e3c0a776bb910116c0e
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-04-10 21:13:08 +0900

    [Feat]: 익스텐션 토글 상태, 클릭 이벤트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    chrome.storage.local.get(['targetOption'], (result) => {
+      const { targetOption } = result;
+
+      if (inputRef.current) {
+        if (targetOption === '_parent') inputRef.current.checked = false;
+        if (targetOption === '_blank') inputRef.current.checked = true;
+      }
+    });
+  }, []);
+
+  const handleToggle = (
+    event: React.MouseEvent<HTMLInputElement, MouseEvent>
+  ) => {
+    const { checked } = event.currentTarget;
+    if (checked) {
+      return chrome.storage.local.set({ targetOption: '_blank' });
+    }
+    chrome.storage.local.set({ targetOption: '_parent' });
+  };
+
   return (
     <div className="container">
-      <input type="checkbox" className="toggle_input" id="toggle" />
+      <input
+        ref={inputRef}
+        type="checkbox"
+        className="toggle_input"
+        id="toggle"
+        onClick={(event) => handleToggle(event)}
+      />
       <label className="toggle_label" htmlFor="toggle">
         <div className="toggle"></div>
         <h4 className="current">Current</h4>
